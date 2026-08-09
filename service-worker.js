a865b099d8b0df5c33d567676167458ad66be0a2
ajayhasdapadmob <ajayhasda623@gmail.com>
2026-08-09 23:08:36 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -1,111 +1,58 @@
-const CACHE_NAME = "apl-tournament-v1";
+const CACHE_NAME = "apl-tournament-v2";
 
 const APP_FILES = [
     "./",
     "./index.html",
-    "./dashboard.html",
-    "./profile.html",
-    "./create-tournament.html",
-    "./my-tournaments.html",
-    "./tournament.html",
-    "./registration.html",
-    "./admin.html",
-    "./schedule.html",
-    "./results.html",
-    "./points.html",
-    "./playerstats.html",
-    "./orange.html",
-    "./purple.html",
-    "./live.html",
-    "./contact.html",
-    "./receipt.html",
     "./manifest.json",
 
-    "./css/style.css",
-    "./css/dashboard.css",
-    "./css/profile.css",
-    "./css/tournament.css",
-    "./css/admin.css"
+    "./images/icon-192.png",
+    "./images/icon-512.png",
+
+    "./css/style.css"
 ];
 
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-            .then(cache => {
-
-                return cache.addAll(APP_FILES);
-
-            })
-
+            .then(cache => cache.addAll(APP_FILES))
     );
 
     self.skipWaiting();
-
 });
 
-
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
         caches.keys().then(keys => {
-
             return Promise.all(
-
                 keys.map(key => {
-
                     if (key !== CACHE_NAME) {
-
                         return caches.delete(key);
-
                     }
-
                 })
-
             );
-
         })
-
     );
 
     self.clients.claim();
-
 });
 
-
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
         fetch(event.request)
             .then(response => {
 
-                const copy =
-                    response.clone();
+                const copy = response.clone();
 
                 caches.open(CACHE_NAME)
                     .then(cache => {
-
-                        cache.put(
-                            event.request,
-                            copy
-                        );
-
+                        cache.put(event.request, copy);
                     });
 
                 return response;
 
             })
             .catch(() => {
-
-                return caches.match(
-                    event.request
-                );
-
+                return caches.match(event.request);
             })
-
     );
-
-});
+});const CACHE_NAME = "apl-tournament-v2";
